a10fe6252fe8f5bf57f088f0ff7c9f4b9fbdc62f
PhamQuyTung <pqthp18903@gmail.com>
2025-06-07 12:10:51 +0700

    Xong UI phần CF-Slider

--- frontend/src/pages/Home/ContainerFluid/CF-Slider/CF-Slider.js
+++ frontend/src/pages/Home/ContainerFluid/CF-Slider/CF-Slider.js
@@ -1,14 +1,39 @@
 import styles from './CF-Slider.module.scss';
 import classNames from 'classnames/bind';
+import Banner1 from '~/assets/images/Slider/Banner1.webp';
+import Banner2 from '~/assets/images/Slider/Banner2.webp';
+import Banner3 from '~/assets/images/Slider/Banner3.webp';
+import Banner4 from '~/assets/images/Slider/Banner4.webp';
+import Banner5 from '~/assets/images/Slider/Banner5.webp';
+import Banner6 from '~/assets/images/Slider/Banner6.webp';
 
 const cx = classNames.bind(styles);
 
 function CFSlider() {
     return (
         <div className={cx('CFSlider')}>
-            <div className={cx('CFSlider-wrap')}>CFSlider</div>
+            <div className={cx('CFSlider-wrap')}>
+                <div className={cx('div15')}>
+                    <img src={Banner1} alt='Banner1' />
+                </div>
+                <div className={cx('div2')}>
+                    <img src={Banner2} alt='Banner2' />
+                </div>
+                <div className={cx('div3')}>
+                    <img src={Banner3} alt='Banner3' />
+                </div>
+                <div className={cx('div13')}>
+                    <img src={Banner4} alt='Banner4' />
+                </div>
+                <div className={cx('div14')}>
+                    <img src={Banner5} alt='Banner5' />
+                </div>
+                <div className={cx('div4')}>
+                    <img src={Banner6} alt='Banner6' />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default CFSlider;
