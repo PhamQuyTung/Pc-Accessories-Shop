60453d4aa07a91c5c987537dfa61a1dadb9c763f
PhamQuyTung <pqthp18903@gmail.com>
2025-06-09 12:44:35 +0700

    Chỉnh lại pagination slider swipper

--- frontend/src/pages/Home/ContainerFluid/CF-Slider/CF-Slider.js
+++ frontend/src/pages/Home/ContainerFluid/CF-Slider/CF-Slider.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './CF-Slider.module.scss';
 import classNames from 'classnames/bind';
 
@@ -9,6 +11,7 @@ import SLider2 from '~/assets/images/Slider/SliderShow/Slider2.webp';
 import SLider3 from '~/assets/images/Slider/SliderShow/Slider3.webp';
 import SLider4 from '~/assets/images/Slider/SliderShow/SLider4.webp';
 import SLider5 from '~/assets/images/Slider/SliderShow/Slider5.webp';
+import SLider6 from '~/assets/images/Slider/SliderShow/Slider6.webp';
 import Banner2 from '~/assets/images/Slider/Banner2.webp';
 import Banner3 from '~/assets/images/Slider/Banner3.webp';
 import Banner4 from '~/assets/images/Slider/Banner4.webp';
@@ -21,44 +24,67 @@ const cx = classNames.bind(styles);
 function CFSlider() {
     return (
         <div className={cx('CFSlider')}>
-            <div className={cx('CFSlider-wrap')}>
-                {/* SliderShow */}
-                <div className={cx('div15')}>
-                    <Swiper {...mainSliderConfig} className={cx('custom-swiper')}>
-                        <SwiperSlide>
-                            <img src={SLider1} alt="Slider1" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={SLider2} alt="Slider2" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={SLider3} alt="Slider3" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={SLider4} alt="Slider4" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={SLider5} alt="Slider5" />
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
+            {/* SliderShow */}
+            <div className={cx('div15')}>
+                <Swiper {...mainSliderConfig} className={cx('custom-swiper')}>
+                    <SwiperSlide>
+                        <Link to="/thu-cu-doi-moi">
+                            <img src={SLider1} alt="Slider1" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to="/laptop-gaming">
+                            <img src={SLider2} alt="Slider2" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to="/laptop-acer-zenbook-14-ux3407qa-qd299ws">
+                            <img src={SLider3} alt="Slider3" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to="laptop-nvidia-rtx-50">
+                            <img src={SLider4} alt="Slider4" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to="/pc-gvn">
+                            <img src={SLider5} alt="Slider5" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to="/man-hinh">
+                            <img src={SLider6} alt="Slider6" className={cx('img-slide')} />
+                        </Link>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
 
-                {/* Banner */}
-                <div className={cx('div2')}>
+            {/* Banner */}
+            <div className={cx('div2')}>
+                <Link to="/lap-top-van-phong">
                     <img src={Banner2} alt="Banner2" />
-                </div>
-                <div className={cx('div3')}>
+                </Link>
+            </div>
+            <div className={cx('div3')}>
+                <Link to="/laptop-gaming">
                     <img src={Banner3} alt="Banner3" />
-                </div>
-                <div className={cx('div13')}>
+                </Link>
+            </div>
+            <div className={cx('div13')}>
+                <Link to="/pc-gvn-intel-i5-4050">
                     <img src={Banner4} alt="Banner4" />
-                </div>
-                <div className={cx('div14')}>
+                </Link>
+            </div>
+            <div className={cx('div14')}>
+                <Link to="/chinh-sach-bang-gia-thu-vga-qua-su-dung">
                     <img src={Banner5} alt="Banner5" />
-                </div>
-                <div className={cx('div4')}>
+                </Link>
+            </div>
+            <div className={cx('div4')}>
+                <Link to="/ban-phim-may-tinh">
                     <img src={Banner6} alt="Banner6" />
-                </div>
+                </Link>
             </div>
         </div>
     );
